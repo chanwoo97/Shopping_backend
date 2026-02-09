0828a048771ddf1dacc97fc208035fb3d9cd0380
chanwoo97 <jcw6223@naver.com>
2026-02-09 16:30:10 +0900

    Cart 엔티티 생성 및 Member, Product 연관 관계 설정

---
+++ src/main/java/com/shopping/shopping_backend_server/domain/Cart.java
@@ -0,0 +1,34 @@
+package com.shopping.shopping_backend_server.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "shopping_cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @Column(nullable = false)
+    private int count;
+
+    private LocalDateTime createdAt;
+
+    // 어떤 회원이 담았는지 연결 (N:1 관계)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Member member;
+
+    // 어떤 상품을 담았는지 연결 (N:1 관계)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
