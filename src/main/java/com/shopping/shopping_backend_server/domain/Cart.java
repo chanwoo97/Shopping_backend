a743bd19513c1300eb4f1cbbe97807821c6ffa3f
chanwoo97 <jcw6223@naver.com>
2026-02-15 18:11:48 +0900

    Cart 엔티티에 JPA Auditing 적용 및 날짜 포맷 설정 - @CreatedDate를 통한 생성 시간 자동화 적용 - DATETIME(0) 설정으로 밀리초 단위 제거 - @JsonFormat을 사용하여 날짜 출력 형식 지정

--- src/main/java/com/shopping/shopping_backend_server/domain/Cart.java
+++ src/main/java/com/shopping/shopping_backend_server/domain/Cart.java
@@ -1,10 +1,15 @@
 package com.shopping.shopping_backend_server.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Builder
 @AllArgsConstructor
@@ -20,7 +25,10 @@ public class Cart {
     @Column(nullable = false)
     private int count;
 
-    private LocalDateTime createdAt;
+    @CreatedDate // 자동 등록으로 변경
+    @Column(updatable = false, columnDefinition = "DATETIME(0)")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime orderDate;
 
     // 어떤 회원이 담았는지 연결 (N:1 관계)
     @ManyToOne(fetch = FetchType.LAZY)
