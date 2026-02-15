d7065990da97ae763fd5644d6cb6f539293e1b30
chanwoo97 <jcw6223@naver.com>
2026-02-15 18:13:41 +0900

    Member 엔티티에 JPA Auditing 적용 및 날짜 포맷 최적화 - @CreatedDate를 통한 가입 시간 자동 기록 기능 추가 - DATETIME(0) 설정으로 밀리초 단위 제거 및 DB 저장 형식 지정 - JSON 출력 시 날짜 가독성을 위한 @JSonFormat 패턴 적용

--- src/main/java/com/shopping/shopping_backend_server/domain/Member.java
+++ src/main/java/com/shopping/shopping_backend_server/domain/Member.java
@@ -1,10 +1,15 @@
 package com.shopping.shopping_backend_server.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class) // 시간 감시자 등록
 @Getter
 @Builder
 @AllArgsConstructor
@@ -23,6 +28,11 @@ public class Member {
     private String email;
     private String phone;
     private String address;
-    private LocalDateTime createdAt;
     private String role; // ROLE_USER, ROLE_ADMIN 등
+
+    @CreatedDate // 생성 시점 자동 기록
+    @Column(updatable = false, columnDefinition = "DATETIME(0)") // 수정 시에는 시간이 안 바뀌게 고정
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createdAt;
+
 }
