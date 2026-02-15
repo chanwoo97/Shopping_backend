a3cdb46bb822e287e76aa97267b18b60d186b332
chanwoo97 <jcw6223@naver.com>
2026-02-15 18:15:06 +0900

    Order 엔티티 주문 시각 자동 기록 기능 적용 - @CreatedDate를 사용하여 주문 시점(orderDate) 자동화 - DB 저장 시 소수점 제거를 위한 DATETIME(0) 설정 추가 - @JsonFormat 패턴 적용으로 주문 일시 출력 가독성 개선

--- src/main/java/com/shopping/shopping_backend_server/domain/Order.java
+++ src/main/java/com/shopping/shopping_backend_server/domain/Order.java
@@ -1,10 +1,15 @@
 package com.shopping.shopping_backend_server.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Builder
 @AllArgsConstructor
@@ -17,6 +22,9 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
+    @CreatedDate // 자동 등록으로 변경
+    @Column(updatable = false, columnDefinition = "DATETIME(0)")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime orderDate;
 
     private int totalPrice;
