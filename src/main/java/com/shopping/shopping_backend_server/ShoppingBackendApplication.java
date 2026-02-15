42debf3968ad2cd15ed9928fcfe61b042f953c7c
chanwoo97 <jcw6223@naver.com>
2026-02-15 18:18:29 +0900

    JPA Auditing 활성화 설정 추가 - 메인 애플리케이션 클래스에 @EnableJpaAuditing 추가 - 엔티티의 생성/수정 시간 자동 기록 기능 활성화

--- src/main/java/com/shopping/shopping_backend_server/ShoppingBackendApplication.java
+++ src/main/java/com/shopping/shopping_backend_server/ShoppingBackendApplication.java
@@ -2,7 +2,9 @@ package com.shopping.shopping_backend_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ShoppingBackendApplication {
 
